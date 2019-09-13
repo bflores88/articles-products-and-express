7b7a96cfab6209bdeab579739a1a73ee670d4ea4
kmietotwy <kmietotwy@gmail.com>
2019-09-13 09:48:07 -0700

    commented out logging middleware to prepare for github pages

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ const articles = require(`./routes/articles.js`);
 const products = require(`./routes/products.js`);
 
 app.engine(
-  '.hbs',
-  exphbs({
-    defaultLayout: 'main',
-    extname: '.hbs',
-    layoutsDir: __dirname + '/views/layouts/',
-    partialsDir: __dirname + '/views/partials/',
-  }),
+	'.hbs',
+	exphbs({
+		defaultLayout: 'main',
+		extname: '.hbs',
+		layoutsDir: __dirname + '/views/layouts/',
+		partialsDir: __dirname + '/views/partials/',
+	}),
 );
 app.set('view engine', '.hbs');
 
@@ -24,41 +24,41 @@ app.use(methodOverride('_method'));
 app.use(express.static('public'));
 
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  }),
+	bodyParser.urlencoded({
+		extended: true,
+	}),
 );
 
-app.use((req, res, next) => {
+// app.use((req, res, next) => {
 
-  let currentDate = new Date().toDateString();
-  let logFile = currentDate.split(' ').join('_');
+//   let currentDate = new Date().toDateString();
+//   let logFile = currentDate.split(' ').join('_');
 
-  fs.appendFile(
-    `./logs/${logFile}.log`,
-    '\n' + `${req.method}_${req.url}_${new Date().toUTCString()}`,
-    'utf8',
-    (err) => {
-      if (err) throw err;
-      return true;
-    },
-  );
-  next();
-});
+//   fs.appendFile(
+//     `./logs/${logFile}.log`,
+//     '\n' + `${req.method}_${req.url}_${new Date().toUTCString()}`,
+//     'utf8',
+//     (err) => {
+//       if (err) throw err;
+//       return true;
+//     },
+//   );
+//   next();
+// });
 
 app.use('/products', products);
 
 app.use('/articles', articles);
 
 app.get('/', (req, res) => {
-  res.render('layouts/home');
+	res.render('layouts/home');
 });
 
 app.use((req, res) => {
-  res.status(404);
-  res.render('layouts/404');
+	res.status(404);
+	res.render('layouts/404');
 });
 
 const server = app.listen(PORT, () => {
-  console.log(`Express app is running at port ${PORT}`);
+	console.log(`Express app is running at port ${PORT}`);
 });
